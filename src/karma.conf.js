ead10a2713454439967f773f6838f625bbabaf5f
swoldetsadick <kebrab67@gmail.com>
2019-11-18 03:52:19 +0100

    remove headless using only PhantomJs for CI

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,9 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -29,16 +26,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'PhantomJS', 'HeadlessChrome'],
-    customLaunchers:{
-      HeadlessChrome:{
-        base: 'ChromeHeadless',
-        flags: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox'
-        ]
-      }
-    },
+    browsers: ['Chrome', 'PhantomJS'],
     singleRun: false,
     restartOnFileChange: true
   });
