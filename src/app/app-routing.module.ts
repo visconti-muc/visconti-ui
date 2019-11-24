176e6f58e6b6676ae8f21c72db7297c27b83d9c9
swoldetsadick <kebrab67@gmail.com>
2019-11-24 12:09:30 +0100

    trying to see if coverage works by deeting login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 /* Component */
-import { LoginComponent } from './login/login.component';
+// import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  // { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
