085635f1eb3cf027f9be5cfd9bb68474eee2b2f3
swoldetsadick <kebrab67@gmail.com>
2019-11-27 20:46:00 +0100

    finishin documenting userModel

--- TODO.md
+++ TODO.md
@@ -1,5 +1,3 @@
-Document constructor of userModel
-
 Document and test globals.service.ts
 
 Rework UserService
--- src/app/shared/models/user.ts
+++ src/app/shared/models/user.ts
@@ -1,10 +1,15 @@
-/**
- * This module describes the class UserModel that is the blueprint from which individual user objects are created.
- */
-
 /* project related imports */
 import { supportedLanguages } from '../services/globals.service';
 
+/**
+ * This module describes the class UserModel that is the blueprint from which individual user objects are created.
+ *
+ * Each instance of UserModel represents a physical user of the visconti ui application, and is defined by a set of
+ * rights on said application. The role property of the user defining the set of rights which applies to specified user.
+ * Each user is defined by:
+ *  - PreferredLanguage: The user's preferred language
+ *  - OtherLanguages: A set of additional languages used by the user
+ */
 export class UserModel {
 
     /**
@@ -16,21 +21,59 @@ export class UserModel {
      */
     private PreferredLanguage: supportedLanguages;
 
+    /**
+     * Constructor for UserModel initiated wether with values parsed out of JSON passed to the constructor, or default
+     * values passed to its properties.
+     * Each instance of UserModel represents a user of the visconti UI application.
+     * @param userResponse A response JSON sent back from server containing user information details.
+     */
     constructor(userResponse: any) {
         this.OtherLanguages = userResponse.otherLanguages || [];
         this.PreferredLanguage = userResponse.preferredLanguage || '';
     }
 
+    /**
+     * This is a simple getter of the OtherLanguages property of UserModel.
+     * @example
+     * Usage:
+     * new UserModel().usersOtherLanguages
+     * @returns An array of strings, two letters long language short code, representing the other languages used by user
+     * except his preferred language.
+     */
     get usersOtherLanguages(): string[] {
         return this.OtherLanguages;
     }
+    /**
+     * This is a simple setter of the otherLanguages property of UserModel.
+     * @example
+     * Usage:
+     * new UserModel().otherLanguages = ['de', 'gr']
+     * @param otherLanguages An array of strings, two letters long language short code, representing the other languages
+     * used by user except his preferred language.
+     */
     set usersOtherLanguages(otherLanguages: string[]) {
         this.OtherLanguages = otherLanguages;
     }
 
+    /**
+     * This is a simple getter of the PreferredLanguage property of UserModel.
+     * @example
+     * Usage:
+     * new UserModel().usersLanguage
+     * @returns A member of enumeration called supportedLanguages which are a set of strings of length 2, two letters
+     * long language short code, representing the preferred language of the user.
+     */
     get usersLanguage(): supportedLanguages {
         return this.PreferredLanguage;
     }
+    /**
+     * This is a simple setter of the PreferredLanguage property of UserModel.
+     * @example
+     * Usage:
+     * new UserModel().usersLanguage = supportedLanguages.fr
+     * @param preferredLanguages A member of enumeration called supportedLanguages which are a set of strings of length
+     * 2, two letters long language short code, representing the preferred language of the user.
+     */
     set usersLanguage(preferredLanguages: supportedLanguages) {
         this.PreferredLanguage = preferredLanguages;
     }
