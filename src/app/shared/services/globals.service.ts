2b8eb17960a66427bb73fed4b95a955dacd04b1a
swoldetsadick <kebrab67@gmail.com>
2019-11-27 21:27:59 +0100

    finished documenting GlobalService

--- src/app/shared/services/globals.service.ts
+++ src/app/shared/services/globals.service.ts
@@ -1,16 +1,29 @@
+/* angular related imports */
 import { Injectable } from '@angular/core';
 
+/**
+ * This service handles variables and methods defined once for here to be used throughout the application without change
+ */
 @Injectable({
     providedIn: 'root'
 })
 export class GlobalsService {
 
     /* ---------------------------------------------- Global variables ---------------------------------------------- */
-    // readonly supportedLanguages = ['de', 'en', 'fr', 'it'];
-
-    // constructor() {}
 
     /* ------------------------------------------ Global Anonymous functions ---------------------------------------- */
+
+    /**
+     * Anonymous function filtering out repeat values from arrays and returning an array of distinct value when passed
+     * to filter function.
+     * @example
+     * Usage:
+     * [0, 1, 2, 3, 3, 4, 4, 4, 2, 5, 6].filter(distinct)
+     *
+     * @param value Value of element of the array being filtered.
+     * @param index Index of element of the array being filtered.
+     * @param self A reference to the array being filtered itself.
+     */
     distinct = <T>(value: T, index: number, self: T[]) => {
         return self.indexOf(value) === index;
     }
@@ -18,6 +31,10 @@ export class GlobalsService {
 }
 
 /* ----------------------------------------------- Global enumerations ---------------------------------------------- */
+
+/**
+ * This enumeration lists of languages supported by the visconti ui application.
+ */
 export enum supportedLanguages {
     'de', 'en', 'fr', 'it'
 }
