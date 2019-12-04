cd013fe894727cc545680c936b15f86866591b45
swoldetsadick <kebrab67@gmail.com>
2019-12-04 22:23:25 +0100

    arranged the userService to have languages stored in senseful manner

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -12,7 +12,7 @@ import { UserModel } from '../models/user';
 import { GlobalsService, supportedLanguages } from './globals.service';
 
 /**
- * This service handles exchange of information of user logged to visconti UI application between different modules
+ * This service handles exchange of information of user logged to Visconti UI application between different modules
  * within said application.
  */
 @Injectable({
@@ -24,8 +24,8 @@ export class UserService {
      * A JSON containing preferredLanguage and otherLanguages fields of JSON.
      */
     readonly defaultUserResponse = {
-        preferredLanguage: this.getNavigatorLanguage(),
-        otherLanguages: this.getAllNavigatorLanguages()
+        preferredLanguage: this.getApplicationLanguage(this.getMainNavigatorLanguage()),
+        otherLanguages: this.filterForOtherLanguages()
     };
     /**
      * Behavior variable that holds the current value of user an instance of class UserModel.
@@ -58,33 +58,60 @@ export class UserService {
 
     /* --------------------------------------------- supporting methods --------------------------------------------- */
     /**
-     * This method extracts all other languages of the user's browser (separate from default browser language), and
-     * returns said languages.
+     * This method extracts all the languages supported by the user's browser language, then gets the user's browser main
+     * language. If the the browser's main language is not supported by the application, then it is added to all the array
+     * of all supported languages. At last, the resulting array of languages is filtered for any mentioned of the
+     * application's default language for the current user. The resulting array is returned.
      *
-     * @returns A list of string (two letters long language short codes) describing  alternatives languages that were
-     * used in the past by users browser.
+     * @returns A string (two letters long language short code) that are all the language of the browser of the user that
+     * is separate of the application's default language for the current user.
+     */
+    public filterForOtherLanguages(): string [] {
+        const allNavigatorLanguages = this.getAllNavigatorLanguages();
+        const navigatorLanguage = this.getMainNavigatorLanguage();
+        const applicationLanguage = this.getApplicationLanguage(navigatorLanguage);
+        if ( applicationLanguage.toString().toLowerCase() !== navigatorLanguage.toLowerCase() ) {
+            allNavigatorLanguages.push(navigatorLanguage.toLowerCase());
+        }
+        return allNavigatorLanguages.filter(( lang ) => {
+            return applicationLanguage.toString().toLowerCase() !== lang;
+        });
+    }
+
+    /**
+     * This method gets a language as input parameter and matches it to the supported languages for this application.
+     * If the language of browser is matched to supported languages, then that language is returned, else English is
+     * returned by default.
+     *
+     * @param mainNavigatorLanguage A string (two letters long language short code) that is representing a language.
+     * @returns A string (two letters long language short code) that is the language passed as parameter (if said
+     * language is in supportedLanguages enum) else supportedLanguages.en.
+     */
+    public getApplicationLanguage(mainNavigatorLanguage: any): supportedLanguages {
+        const index = Object.keys(supportedLanguages).indexOf(mainNavigatorLanguage);
+        return index > -1 ? mainNavigatorLanguage as supportedLanguages : supportedLanguages.en;
+    }
+
+    /**
+     * This method extracts all languages of the user's browser and returns them as strings in an Array.
+     *
+     * @returns A list of string (two letters long language short codes) describing languages installed in the user's
+     * browser.
      */
     public getAllNavigatorLanguages(): string [] {
         let languages: any;
         languages = window.navigator.languages || [];
-        return languages.map((data) => data.split('-')[0])
-            .map((language) => {
-                const index = Object.keys(supportedLanguages).indexOf(language);
-                return index > -1 ? '' : language.toString();
-            })
+        return languages.map((data) => data.split('-')[0].toString().toLowerCase())
             .filter(this.globalsService.distinct)
             .filter(this.globalsService.empty);
     }
 
     /**
-     * This method extracts the user's browser language and matches it to the supported languages for this application.
-     * If the language of browser is matched to supported languages, then that language is returned, else English is
-     * returned by default.
+     * This method extracts the user's browser main language.
      *
-     * @returns A string (two letters long language short code) that is the language of the browser of the user (if said
-     * language is in supportedLanguages enum) else supportedLanguages.en.
+     * @returns A string (two letters long language short code) that is the default language of the browser of the user.
      */
-    public getNavigatorLanguage(): supportedLanguages {
+    public getMainNavigatorLanguage(): string {
         let locale;
         let navigator: any;
         navigator = window.navigator;
@@ -93,8 +120,7 @@ export class UserService {
         } catch (e) {
             locale = navigator.userLanguage;
         }
-        const index = Object.keys(supportedLanguages).indexOf(locale.split('-')[0]);
-        return index > -1 ? locale.toString() as supportedLanguages : supportedLanguages.en;
+        return locale.split('-')[0].toString().toLowerCase();
     }
 
 }
